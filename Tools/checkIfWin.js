73ac2c6cc72f4e0ed88140f3c819edb6ca505cab
Kamil <kamilzajac01@gmail.com>
2022-12-28 12:01:51 +0100

    Adding drawline

--- Tools/checkIfWin.js
+++ Tools/checkIfWin.js
@@ -1,24 +1,33 @@
 export function checkIfWin(b) {
   let winner = "";
+  //I need winWay just to animate drawing line
+  let winWay = "";
   for (let i = 0; i < 3; i++) {
     //horizontal
-    if (b[i][0] === b[i][1] && b[i][1] === b[i][2]) {
+    if (b[i][0] === b[i][1] && b[i][1] === b[i][2] && b[i][0] !== "") {
       winner = b[i][2];
+      winWay = { way: "horizontal", indx: i };
+      break;
     }
   }
   //vertical
   for (let i = 0; i < 3; i++) {
-    if (b[0][i] === b[1][i] && b[1][i] === b[2][i]) {
+    if (b[0][i] === b[1][i] && b[1][i] === b[2][i] && b[0][i] !== "") {
       winner = b[2][i];
+      winWay = { way: "vertical", indx: i };
+      break;
     }
   }
   //diagonal
-  if (
-    (b[0][0] === b[1][1] && b[1][1] === b[2][2]) ||
-    (b[0][2] === b[1][1] && b[1][1] === b[2][0])
-  ) {
+  //rotate is to which direction to rotate the drawing line
+  if (b[0][0] === b[1][1] && b[1][1] === b[2][2] && b[0][0] !== "") {
     winner = b[1][1];
+    winWay = { way: "diagonal", rotate: "-45deg" };
+  }
+  if (b[0][2] === b[1][1] && b[1][1] === b[2][0] && b[0][2] !== "") {
+    winner = b[1][1];
+    winWay = { way: "diagonal", rotate: "45deg" };
   }
 
-  return winner;
+  return { winner: winner, winWay: winWay };
 }
--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 import { createCircle } from "./Tools/createCircle.js";
 import { checkIfWin } from "./Tools/checkIfWin.js";
 
+const trn = document.getElementById("turn");
+const liArray = document.getElementsByTagName("li");
+const table = document.getElementById("table");
+console.log(table.clientHeight);
 let turn = "X";
+trn.textContent = turn;
+
 let board = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
 ];
+
+function animateWin(winWay) {
+  const div = document.createElement("div");
+  //Horizontal case
+  if (winWay.way === "horizontal") {
+    const tHeight = table.clientHeight;
+    const drawLineHeight = (tHeight / 3) * winWay.indx + tHeight / 6;
+    div.classList.add("drawline", "horizontal");
+    div.style.top = drawLineHeight + "px";
+  } else if (winWay.way === "vertical") {
+    const tWidth = table.clientWidth;
+    const drawLineWidth = (tWidth / 3) * winWay.indx + tWidth / 6;
+    div.classList.add("drawline", "vertical");
+    div.style.left = drawLineWidth + "px";
+  } else if (winWay.way === "diagonal") {
+    div.classList.add("drawline", "diagonal");
+    div.style.transform = `translate(-50%, -50%) rotate(${winWay.rotate})`;
+  }
+
+  table.append(div);
+}
 
 const getEmptyCells = () => {
   const arr = [];
@@ -17,11 +44,6 @@ const getEmptyCells = () => {
   }
   return arr;
 };
-
-const trn = document.getElementById("turn");
-trn.textContent = turn;
-
-const liArray = document.getElementsByTagName("li");
 
 for (let i = 0; i < liArray.length; i++) {
   liArray[i].addEventListener("click", playerAction);
@@ -40,7 +62,6 @@ function playerAction(e) {
 }
 function computerAction() {
   const emptyCellsArr = getEmptyCells();
-  //   const randomCell = Math.floor(Math.random() * countEmptyCells().length + 1);
   const randomCellIndex = Math.floor(Math.random() * emptyCellsArr.length);
   const randomCell = emptyCellsArr[randomCellIndex];
   const randomId = randomCell[0] * 3 + (randomCell[1] % 3);
@@ -51,22 +72,21 @@ function computerAction() {
 
 function changeTurn() {
   const winner = checkIfWin(board);
-  if (winner) {
-    trn.textContent = "WINNER" + " " + winner;
+  console.log(winner);
+  if (winner.winner) {
     turn = "";
-    return;
-  }
+    animateWin(winner.winWay);
 
-  if (getEmptyCells().length === 0) {
+    trn.textContent = "WINNER" + " " + winner.winner;
+    return;
+  } else if (getEmptyCells().length === 0) {
     trn.textContent = "THE END";
     return;
-  }
-  if (turn === "X") {
+  } else if (turn === "X") {
     turn = "O";
-    setTimeout(() => {
-      computerAction();
-    }, 1000);
-  } else turn = "X";
+
+    computerAction();
+  } else if (turn === "O") turn = "X";
   trn.textContent = turn;
 }
 
